a4eb07e664b0ee41212159439ff7e2bc86e2478a
Thiago Santana <thiago91098@gmail.com>
2022-12-10 11:42:12 -0300

    first commit

---
+++ src/test/java/br/com/tsantana/employees/EmployeesApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tsantana.employees;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
